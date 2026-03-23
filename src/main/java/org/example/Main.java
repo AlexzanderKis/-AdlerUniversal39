b8c40fc9d31c7c2dc1d21c4a133199c6e74c5cfb
Alex <allfreetome@gmail.com>
2026-03-23 15:40:46 +0300

    KDK 12 calling KDK 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner readScan = new Scanner(System.in);
+        System.out.println("Hello and welcome to Overlook hotel!");
+
+        while(true) {
+            if (readScan.hasNext("q")) { break; }
+            for (int i = 0; i <= 1; i++) {
+                System.out.println("All work and no play makes Jack a dull boy.");
+            }
+        }
+        System.out.println("DAAAAANNYYY");
+    }
+}
