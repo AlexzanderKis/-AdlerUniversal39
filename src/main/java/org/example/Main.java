b28f378338c7678bcebaf2dfc741fadbe19e88a3
Alexander <allfreetome@gmail.com>
2026-03-24 22:35:31 +0300

    KDK 12 calling KDK 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
+import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello and welcome to Overlook hotel!");
-        String readScan = new Scanner(System.in).next();
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to Overlook hotel");
+        String readNameLine = new Scanner(System.in).next();
         while (true) {
-            int rndInt = ThreadLocalRandom.current().nextInt(200, 2000 + 1);
-            System.out.println("All work and no play makes " + readScan + " a dull boy. " + rndInt+"ms "+rndInt+1252+" rndNum "+rndInt+150032+" rnd seed");
             try {
-                Thread.sleep(rndInt);
+                String textLine = "All work and no play makes " + readNameLine + " a dull boy.\n";
+                for (int i = 0; i < textLine.length(); i++) {
+                    int rndDelay = ThreadLocalRandom.current().nextInt(0, 1000 + 1);
+                    Thread.sleep(rndDelay);
+                    System.out.print(textLine.charAt(i));
+                }
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                e.fillInStackTrace();
             }
         }
     }
