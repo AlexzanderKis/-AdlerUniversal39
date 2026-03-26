a4fb0a869cf4163c88c0844816543caa7c0152bd
Alexander <allfreetome@gmail.com>
2026-03-26 22:31:47 +0300

    KDK 12 calling KDK 1

--- src/main/java/org/example/TypeController.java
+++ src/main/java/org/example/TypeController.java
@@ -8,15 +8,13 @@ public class TypeController implements Runnable {
     @Override
     public void run() {
         String readNameLine = new Scanner(System.in).next();
+        String textLine = "All work and no play makes " + readNameLine + " a dull boy.\n"; // строка с текстом и именем которое вводит пользователь
         while (true) { // цикл печати
             try {
-//                AtomicReference<String> textLine = new AtomicReference<>("All work and no play makes " + readNameLine + " a dull boy.\n");
-//                char[] chars = textLine.toCharArray();
-                String textLine = "All work and no play makes "+readNameLine+" a dull boy.\n"; // строка с текстом и именем которое вводит пользователь
-                for (int i = 0; i < textLine.length(); i++){ // цикл с рандомной задержкой печати
+                for (int i = 0; i < textLine.length(); i++) { // цикл с рандомной задержкой печати
                     int rndDelay = ThreadLocalRandom.current().nextInt(30, 500 + 1);
                     Thread.sleep(rndDelay);
-                    System.out.print(textLine.charAt(i));
+                    System.out.print(randomMistypingGen(textLine));
                 }
             } catch (InterruptedException e) {
                 e.fillInStackTrace();
@@ -24,21 +22,24 @@ public class TypeController implements Runnable {
         }
     }
 
-//    public String randomMistyping() {
-//        StringBuilder stringBuilder = new StringBuilder(textLine);
-//        Random random = new Random();
+    public String randomMistypingGen(String textLine) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
 //        String pool = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm[];,./{}:'<>?";
-//        for (int j = 0; j < 4; j++) {
-//            int index = random.nextInt(stringBuilder.length());
-//            char rndChar = pool.charAt(random.nextInt(pool.charAt(index)));
-//            stringBuilder.setCharAt(index, rndChar);
-//        }
-//        return pool;
-//        char[] chars = textLine.toCharArray();
-//        for (char c : chars) {
-//            int rndDelay = ThreadLocalRandom.current().nextInt(30, 500 + 1);
-//            Thread.sleep(rndDelay);
-//            System.out.print(textLine.charAt(c));
-//        }
-//    }
-//}
+//        int index = random.nextInt(stringBuilder.length());
+        char[] chars = textLine.toCharArray();
+        for (char c : chars) {
+            int chance = random.nextInt(100); // Генерирует от 0 до 99
+            if (chance >= 98.5) { // % на ошибку
+                int errorType = random.nextInt(2); // типы ошибок
+                switch (errorType) {
+                    case 0 -> stringBuilder.insert(c, c); // TODO: Двойное нажатие (например, 'k' превращается в 'kk')
+                    case 1 -> stringBuilder.append(c); // TODO: Пропуск буквы (буква просто не печатается)
+                }
+            } else {
+                stringBuilder.append(c); // Добавляем правильную букву
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
