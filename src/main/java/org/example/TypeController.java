7494a8a9ba23231ec0492ba8949058b8904d9af0
Alexander <allfreetome@gmail.com>
2026-03-28 00:05:04 +0300

    KDK 12 calling KDK 1

--- src/main/java/org/example/TypeController.java
+++ src/main/java/org/example/TypeController.java
@@ -12,9 +12,9 @@ public class TypeController implements Runnable {
         while (true) { // цикл печати
             try {
                 for (int i = 0; i < textLine.length(); i++) { // цикл с рандомной задержкой печати
-                    int rndDelay = ThreadLocalRandom.current().nextInt(30, 500 + 1);
+                    int rndDelay = ThreadLocalRandom.current().nextInt(30, 200 + 1);
                     Thread.sleep(rndDelay);
-                    System.out.print(randomMistypingGen(textLine));
+                    System.out.print(randomMistypingGen(String.valueOf(textLine.charAt(i))));
                 }
             } catch (InterruptedException e) {
                 e.fillInStackTrace();
@@ -26,16 +26,30 @@ public class TypeController implements Runnable {
         StringBuilder stringBuilder = new StringBuilder();
         Random random = new Random();
 //        String pool = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm[];,./{}:'<>?";
-//        int index = random.nextInt(stringBuilder.length());
+//        char[] poolChars = pool.toCharArray();
+//        for (char p : poolChars) {
+//        int index = random.nextInt(poolChars.length);
         char[] chars = textLine.toCharArray();
-        for (char c : chars) {
-            int chance = random.nextInt(100); // Генерирует от 0 до 99
-            if (chance >= 98.5) { // % на ошибку
-                int errorType = random.nextInt(2); // типы ошибок
+        for (char c : chars) { // проходим по массиву из букв
+            int errorChance = random.nextInt(1,100); // Генерирует от 0 до 99
+            if (errorChance >= 50) { // % на ошибку
+                int errorType = random.nextInt(4); // типы ошибок
                 switch (errorType) {
-                    case 0 -> stringBuilder.insert(c, c); // TODO: Двойное нажатие (например, 'k' превращается в 'kk')
-                    case 1 -> stringBuilder.append(c); // TODO: Пропуск буквы (буква просто не печатается)
-                }
+                    case 0 -> { // Двойное нажатие (например, 'k' превращается в 'kk')
+                        if (textLine.equals(" ") || textLine.equals(".") || textLine.equals("\n")) {
+                            stringBuilder.append(c);
+                        } else { stringBuilder.repeat(c, 2); } }
+                    case 1 -> { // Пропуск буквы (буква просто не печатается)
+                        if (!stringBuilder.isEmpty() || textLine.equals(".")) { // проверка если stringBuilder не пустой
+                            int charPos = random.nextInt(textLine.length()); // берём рандомную букву на всей длине stringBuilder
+                            stringBuilder.deleteCharAt(charPos); // удаляем букву на рандомной позиции
+                        } else { stringBuilder.append(c); } }
+                    case 3 -> { // Переводит рандомную букву в заглавную
+                        if (!stringBuilder.isEmpty() && Character.isLetter(c) != textLine.equals(" ") || textLine.equals(".") || textLine.equals("\n")) {
+                            int charPos = random.nextInt(textLine.length());
+                            stringBuilder.setCharAt(charPos, Character.toUpperCase(c));
+                        } stringBuilder.append(c); }
+                    }
             } else {
                 stringBuilder.append(c); // Добавляем правильную букву
             }
