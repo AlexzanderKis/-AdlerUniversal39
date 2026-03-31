a3d64433d78eda612c847e8db1d3793d39cdd642
Alexander <allfreetome@gmail.com>
2026-03-31 21:17:54 +0300

    KDK 12 calling KDK 1

---
+++ src/main/java/com/example/AdlerUniversal39/AdlerUniversal39Application.java
@@ -0,0 +1,13 @@
+package com.example.AdlerUniversal39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdlerUniversal39Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdlerUniversal39Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/AdlerUniversal39/TypeController.java
@@ -0,0 +1,7 @@
+package com.example.AdlerUniversal39;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TypeController {
+}
